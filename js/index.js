7b6f66e30c2f6d2333d3b1d746296ede562aceb9
Pradham K <email@pradhamk.com>
2023-12-03 15:41:27 -0800

    mobile circle click

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
-window.addEventListener('click', (e) => {
+function click_circle(mx, my) {
     const circle = document.getElementById('circle');
-    circle.style.left = e.clientX - 50 + 'px';
-    circle.style.top = e.clientY - 45 + 'px';
+    circle.style.left = mx - 50 + 'px';
+    circle.style.top = my - 45 + 'px';
     circle.classList.add('circleglow');
     setTimeout(() => { circle.classList.remove('circleglow') }, 500);
-})
+}
+
+window.addEventListener('click', (e) => { click_circle(e.clientX, e.clientY) })
+window.addEventListener('touchstart', (e) => { click_circle(e.touches[0].clientX, e.touches[0].clientY) })
